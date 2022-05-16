31ac6c2c0b7b084115aea22aef920116b7acc696
Henni Djalel <82578209+hennidjalel@users.noreply.github.com>
2022-05-16 17:49:36 +0200

    create json server

--- src/home.jsx
+++ src/home.jsx
@@ -1,31 +1,56 @@
-import { useState } from "react";
-import uniqid from "uniqid";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+
 const Home = () => {
-    // const [name, setName] = useState('mario');
-    // const [age, setAge] = useState(25);
 
-    // const handClick = () => {
-    //     setName('Luigi');
-    //     setAge(30);
-    // }
-    // const handClickAgain = () => {
-    //     setAge(30)
-    // }
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handlClick = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    };
 
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new websit', body: 'lorem ipsi=um...', author: 'mario', id: 1 },
-        { title: 'Welcom party!', body: 'lorem ipsi=um...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top ninja', body: 'lorem ipsi=um...', author: 'mario', id: 3 }
-    ])
 
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(response => response.json())
+                .then(data => {
+                    setBlogs(data);
+                    setIsLoading(false)
+                })
+        }, 4000);
+    }, []);
     return (
         <div className="Home">
-            <BlogList blogs={blogs} title="All blogs!" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />
+            {isLoading && <div class="spinner">
+                <span>L</span>
+                <span>O</span>
+                <span>A</span>
+                <span>D</span>
+                <span>I</span>
+                <span>N</span>
+                <span>G</span>
+            </div>}
+            {/* first blogs is the props name, {blogs} is the data we pass in  */}
+            {blogs && < BlogList blogs={blogs} title="All blogs!" handlClick={handlClick} />}
+
+
         </div>
     );
-}
+};
 
 export default Home;
+
+// const [name, setName] = useState('mario');
+//     const [age, setAge] = useState(25);
+
+//     constandCli hck = () => {
+//         setName('Luigi');
+//         setAge(30);
+//     }
+//     const handClickAgain = () => {
+//         setAge(30)
+//     }
